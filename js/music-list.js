3f46f7f4f344d2cf914fc6b8cbc0d433650d7ed7
Leonidah-nyaboke <114716405+Leonidah-nyaboke@users.noreply.github.com>
2023-07-27 02:44:27 +0300

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Sun Goes Down",
+    artist: "Jasmine Thompson",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Swimming in Stars",
+    artist: "Wayfarers",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Lose You Too",
+    artist: "Shy Martin",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Never Say Never",
+    artist: "The Fray",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Uncover",
+    artist: "Zara Larsson",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Family",
+    artist: "The Chainsmokers",
+    img: "music-6",
+    src: "music-6"
+  },
+ ];
